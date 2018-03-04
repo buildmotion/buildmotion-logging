5ac24366ad0ad06e6a8f6a67e108820416f2a14c
joe.gorilla <matt@hybridmobileapp.net>
2018-03-04 10:20:42 -0700

    build/deploy changes

--- dist/buildmotion-logging.module.js
+++ dist/buildmotion-logging.module.js
@@ -1,22 +1,21 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+import { NgModule, ModuleWithProviders, NO_ERRORS_SCHEMA } from "@angular/core";
 var BuildMotionLoggingModule = /** @class */ (function () {
     function BuildMotionLoggingModule() {
     }
-    BuildMotionLoggingModule = __decorate([
-        NgModule({
-            imports: [],
-            declarations: [],
-            exports: [],
-            schemas: [NO_ERRORS_SCHEMA]
-        })
-    ], BuildMotionLoggingModule);
     return BuildMotionLoggingModule;
 }());
 export { BuildMotionLoggingModule };
+function BuildMotionLoggingModule_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    BuildMotionLoggingModule.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    BuildMotionLoggingModule.ctorParameters;
+}
 //# sourceMappingURL=buildmotion-logging.module.js.map
--- dist/logging.service.js
+++ dist/logging.service.js
@@ -1,14 +1,12 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-import { Injectable } from '@angular/core';
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+import { Injectable, Optional } from "@angular/core";
+import { loggingServiceConfig } from "./logging.service.config";
+import { Severity } from "./severity.enum";
 var LoggingService = /** @class */ (function () {
     function LoggingService() {
-        this.applicationName = 'Angularlicio.us';
-        this.serviceName = 'LoggingService';
     }
     /**
      * The [LoggingService] constructor.
@@ -33,18 +31,60 @@ var LoggingService = /** @class */ (function () {
      * @param severity
      * @param message
      */
-    LoggingService.prototype.log = function (source, severity, message) {
+    /**
+     * Use this method to send a log message with severity and source information
+     * to the application's logger.
+     *
+     * If the application environment mode is [Production], the information will
+     * be sent to a centralized repository.
+     *
+     * @param {?} source
+     * @param {?} severity
+     * @param {?} message
+     * @return {?}
+     */
+    LoggingService.prototype.log = /**
+     * Use this method to send a log message with severity and source information
+     * to the application's logger.
+     *
+     * If the application environment mode is [Production], the information will
+     * be sent to a centralized repository.
+     *
+     * @param {?} source
+     * @param {?} severity
+     * @param {?} message
+     * @return {?}
+     */
+    function (source, severity, message) {
         this.source = this.applicationName + "." + source;
         this.severity = severity;
         this.message = message;
         this.timestamp = new Date();
-        var msg = "" + this.message;
+        var /** @type {?} */ msg = "" + this.message;
         console.log(this.severity + " from " + this.source + ": " + msg + " (" + this.timestamp + ")");
     };
-    LoggingService = __decorate([
-        Injectable()
-    ], LoggingService);
     return LoggingService;
 }());
 export { LoggingService };
+function LoggingService_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    LoggingService.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    LoggingService.ctorParameters;
+    /** @type {?} */
+    LoggingService.prototype.applicationName;
+    /** @type {?} */
+    LoggingService.prototype.serviceName;
+    /** @type {?} */
+    LoggingService.prototype.source;
+    /** @type {?} */
+    LoggingService.prototype.severity;
+    /** @type {?} */
+    LoggingService.prototype.message;
+    /** @type {?} */
+    LoggingService.prototype.timestamp;
+}
 //# sourceMappingURL=logging.service.js.map
